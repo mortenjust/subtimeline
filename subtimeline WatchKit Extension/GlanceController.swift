80896bc521149ccab8969d4266de7fa53cdedb78
mortenjust <morten.just@genstart.dk>
2015-12-29 11:34:19 -0800

    Initial Commit

---
+++ subtimeline WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  subtimeline WatchKit Extension
+//
+//  Created by Morten Just Petersen on 12/29/15.
+//  Copyright © 2015 Morten Just Petersen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
